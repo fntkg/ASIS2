8d06d61248c982e5f9b486c51d4c84c783f23660
German <garces.g1999@gmail.com>
2020-04-05 14:49:44 +0200

    Añadida gestion de maquinas y grupos para cada instruccion

--- trabajo_admin_distribuida/u.rb
+++ trabajo_admin_distribuida/u.rb
@@ -1,51 +1,134 @@
 #!/usr/bin/ruby
 
+# Para los ordenadores del laboratorio:
+## #!/usr/bin/env ruby
+## chmod a+x u.rb
+
 require 'net/ping'
 require 'net/ssh'
-
 
 #####################################
 #        Funciones auxiliares       #
 #####################################
-def p
-  #ejecutar estilo ping al puerto 22 a todas las máquinas del conjunto
-  puts ">>Leyendo direcciones de las maquinas"
-  File.foreach(ENV['HOME'] + '/.u/hosts') do |direccion|
-    direccion = direccion.chop #QUITAR SALTO DE LINEA
-    check = Net::Ping::External.new(direccion, 22, 0.1)
-    if check.ping?
-      puts direccion + ": FUNCIONA"
+
+def ping(direccion)
+  check = Net::Ping::External.new(direccion, 22, 0.1)
+  if check.ping?
+    puts direccion + ": FUNCIONA"
+  else
+    puts direccion + ": falla"
+  end
+end
+
+def ssh(direccion, command)
+  # Para evitar timeouts y excepciones se hace PING antes de ssh.
+  check = Net::Ping::External.new(direccion, 22, 0.1)
+  if check.ping?
+    Net::SSH.start(direccion, 'a757024', password: "Egdxwa") do |ssh|
+      output = ssh.exec!(command)
+      puts direccion + ": exito"
+      puts output
+      puts
+    end
+  else
+    puts direccion + ": falla"
+    puts
+  end
+end
+
+# @who = a quien mandar el comando @comando
+def who(who, comando, command)
+  if who == "all"
+    # Recorrer todas las maquinas
+    File.foreach(ENV['HOME'] + '/.u/hosts') do |direccion|
+      direccion = direccion.chop #QUITAR SALTO DE LINEA
+      # Pasar a siguiente iteracion si es linea en blanco o nombre de grupo.
+      next if direccion.match(/^-.*$/m) || direccion.match(/^$/m) || direccion.match(/^\+.*$/m)
+      if comando == "p"
+        ping(direccion)
+      elsif comando == "s"
+        ssh(direccion, command)
+      end
+    end
+  else
+    # Hay que recorrer un grupo o una maquina en concreto
+    if (File.read(ENV['HOME'] + '/.u/hosts').include?("-"+who))
+      # Caso de que exista un grupo con ese nombre.
+      leer = false
+      File.foreach(ENV['HOME'] + '/.u/hosts') do |direccion|
+        direccion = direccion.chop #QUITAR SALTO DE LINEA
+        if direccion == "-"+who
+          # Si la linea es el nombre del grupo
+          # Indicar que las siguientes direcciones son las que hay que leer
+          leer = true;
+        else
+          break if leer && direccion == ""
+          if leer
+            # Si estamos en una maquina del grupo
+            if direccion.match(/^\+.*$/m)
+              # Si hay llamadas a otros grupos
+              who(direccion.sub(/^\+/, ""), comando, command)
+            else
+              if (comando == "p")
+                ping(direccion)
+              elsif comando == "s"
+                ssh(direccion, command)
+              end
+            end
+            # Si hay llamadas a otros grupos
+          end
+        end
+      end
     else
-      puts direccion + ": falla"
+      # Caso de que sea una máquina en concreto
+      if (comando == "p")
+        ping(who)
+      elsif comando == "s"
+        ssh(who, command)
+      end
     end
   end
 end
 
-def c(comando)
-  #ejecutar comando remoto mediante ssh en todo el conjunto de máquinas
-  puts ">>Leyendo direcciones de las maquinas"
-  File.foreach(ENV['HOME'] + '/.u/hosts') do |direccion|
-    direccion = direccion.chop #QUITAR SALTO DE LINEA
-    # Para evitar timeouts y excepciones se hace PING antes de ssh.
-    check = Net::Ping::External.new(direccion, 22, 0.1)
-    if check.ping?
-      Net::SSH.start(direccion, 'a757024', password: "Egdxwa") do |ssh|
-        output = ssh.exec!(comando)
-        puts direccion + ": exito"
-        puts output
-      end
-    else
-      puts direccion + ": falla"
-    end
-  end
+
+# COMANDO P
+def p(maquinas)
+  who(maquinas, "p", "")
 end
+
+def s(maquinas, comando)
+  #ejecutar comando remoto mediante ssh en todo el conjunto de máquinas
+  who(maquinas, "s", comando)
+end
+
 #####################################
 #        Funcion principal          #
 #####################################
 
-#Primero cojer las dos opciones posibles
+#Primero cojer las opciones posibles
 option = ARGV[0]
 
+if option == "s" || option == "p"
+  # Comando para todas las maquinas.
+  if option == "p"
+    # Comando p
+    p("all")
+  elsif option == "s"
+    # Comando case
+    # Tomar el comando a realizar mediante ssh del 2º parametro
+    s("all", ARGV[1])
+  end
+else
+  # Comando para un GRUPO o una maquina en concreto
+  option_1 = ARGV[1]
+  if option_1 == "p"
+    p(option)
+  elsif option_1 == "s"
+    s(option, ARGV[2])
+  end
+end
+
+=begin
 if option != "p" && option != "s"
   #Comandos no validos
   puts "Opcion no valida."
@@ -67,3 +150,4 @@ else
     puts "Archivo ~/.u/hosts no encontrado"
   end
 end
+=end
