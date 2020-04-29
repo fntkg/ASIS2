ef87ddb49719717e6fba311798c51758cf3cb0a7
Germán Garcés <757024@unizar.es>
2020-04-29 11:34:03 +0200

    Update ntp_cliente.pp

--- trabajo_admin_distribuida/manifiestos/ntp_cliente.pp
+++ trabajo_admin_distribuida/manifiestos/ntp_cliente.pp
@@ -1,7 +1,25 @@
 # Configuracion clientes NTP
 
-node default {
-  class { 'ntp':
-        servers => ['2001:470:736b:7ff::2']
-  }
+$string = "server 2001:470:736b:7ff::2"
+
+case $operatingsystem {
+        openbsd: {
+                $service = 'ntpd'
+                $file = '/etc/ntpd.conf'
+        }
+        ubuntu: {
+                $service = 'npt'
+                $file = '/etc/ntp.conf'
+         }
+}
+
+file { $file:
+        ensure => file,
+        content => $string
+}
+
+service { $service:
+        ensure => running,
+        enable => true,
+        subscribe => File[$file]
 }
